a1a969ef2a78d358dad752564ad25620820c93f6
CristianC707 <cristian.e.cuevas707@gmail.com>
2021-11-15 15:26:23 -0500

    Got server up and running. Take note of mongoose.set

--- server/index.js
+++ server/index.js
@@ -2,3 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://Cristian-admin:cuevasBOY707@cluster0.pdful.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+    // mongoose.set('useFindAndModify', false);
+
+// https://www.mongodb.com/cloud/atlas
