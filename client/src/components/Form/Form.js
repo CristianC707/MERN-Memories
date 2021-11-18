766e0b904732726295a69f685155bf8b996ed869
CristianC707 <cristian.e.cuevas707@gmail.com>
2021-11-17 23:31:04 -0500

    Fixed button but need to fix margin for submit and choose file.

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -28,7 +28,7 @@ const Form = () => {
                 <div className={classes.file}>
                     <FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...setPostData, selectedFile: base64 })} />
                 </div>
-                <Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
                 <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
             </form>
 
