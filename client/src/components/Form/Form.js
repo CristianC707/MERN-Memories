42a199019e41d2130d4d5cfb45e3b3db5b92a71c
CristianC707 <cristian.e.cuevas707@gmail.com>
2021-11-18 00:01:02 -0500

    Finished adding posts.

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
-import FileBase from 'react-file-base64'
+import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
+
 import useStyles from './style';
+import { createPost } from "../../api/actions/posts";
 
 
 const Form = () => {
     const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
-
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        dispatch(createPost(postData));
     }
 
     const clear = () => {
@@ -25,7 +30,7 @@ const Form = () => {
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...setPostData, title: e.target.value })} />
                 <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...setPostData, message: e.target.value })} />
                 <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...setPostData, tags: e.target.value })} />
-                <div className={classes.file}>
+                <div className={classes.fileInput}>
                     <FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...setPostData, selectedFile: base64 })} />
                 </div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
